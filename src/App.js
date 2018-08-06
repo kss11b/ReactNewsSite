dd8c0bec0692130e57b63a0701de49c2cb6a5401
Kyle S <kstainsby@sbcglobal.net>
2018-08-05 19:43:05 -0500

    React material ui partially implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Click from './click.js'
 import TechNews from './techNews.js'
+import AppBar from '@material-ui/core/AppBar';
 import './App.css';
 
 const MainMenu = () => {return(
@@ -16,7 +17,7 @@ const MainMenu = () => {return(
         Counter
       </button>
     </Link>
-    <Link to="/clock">
+    <Link to="/tech">
       <button>
         Tech
       </button>
@@ -30,51 +31,22 @@ const Home = () => {return(
   </h1>
 )}
 
-// const About = () => {return(
-//   <h1>
-//     About
-//   </h1>
-// )}
-
-// class About extends Component {
-
-  // state = {
-  //   count: 0,
-  // }
-
-//   constructor(props) {
-//   super(props);
-//   state = {count: 0};
-// }
-//
-// increaseCount = () => {
-//     this.setState({count: this.state.count + 1})
-//   }
-//
-//   render(){
-//     return (
-//       <h1>About</h1>
-//       <button onClick={this.increaseCount}>{count}</button>
-//     )
-//   }
-// }
-
-
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Various React Demos</h1>
+          <AppBar position="static" color="default">
+          {/* <header className="App-header"> */}
+            <h1 className="App-title">Cruise News</h1>
             <MainMenu />
-          </header>
-          <div>
+          {/* </header> */}
+        </AppBar>
             <Route exact path="/home" component={Home}/>
             <Route exact path="/counter" component={Click}/>
-            <Route exact path="/clock" component={TechNews}/>
-          </div>
+            <Route exact path="/tech" component={TechNews}/>
+
         </div>
       </Router>
     );
--- src/techNews.js
+++ src/techNews.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import {getTechArticles} from './actions/index.js'
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid';
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
 import './App.css';
 
 class TechNews extends Component {
-
 
 componentDidMount() {
   console.log('tech mounted')
@@ -15,7 +23,19 @@ componentDidMount() {
 createArticleCards = articles => {
   console.log('map articles', articles.articles)
   return articles.map(x =>
-    <li><h1>{x.title}</h1><p>{x.description}</p><a href={x.url}>Read More</a></li>
+    <Grid item xs={4} key={x.title}>
+      <Card className='articleCards'>
+        {/* <CardMedia
+          // image={x.urlToImage}
+          height={0}
+          src={x.urlToImage}
+          title="Contemplative Reptile"
+        /> */}
+        <CardContent className='articleCards'>
+          <Typography gutterBottom variant="headline" component="h2" key={x.title}>{x.title}</Typography><Typography component="p">{x.description}</Typography><Button type="button" href={x.url}>Read More</Button>
+        </CardContent>
+      </Card>
+  </Grid>
   )
 }
 
@@ -27,14 +47,17 @@ createArticleCards = articles => {
     const { techArticles } = this.props;
     return (
       <div>
-      <h1>Tech Articles</h1>
-          <ul>
-            {
-            techArticles ? this.createArticleCards(techArticles) : null
-            // techArticles ? techArticles.map(x => <h1>{x.title}</h1>) : null
-            }
-        </ul>
-      </div>
+      <Grid container justify='center' spacing={8}>
+        <Grid item xs={12}>
+          <h1>Tech Articles</h1>
+        </Grid>
+      </Grid>
+      <Grid container direction="row" justify="center" alignItems="stretch">
+        {
+        techArticles ? this.createArticleCards(techArticles) : <CircularProgress size={50}/>
+        }
+    </Grid>
+  </div>
     )
   }
 }
